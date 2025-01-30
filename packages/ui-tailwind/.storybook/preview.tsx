8d26b7442aea0529600b783b340ebfcdcc9cb583
Grécio Diogo <fonebahia8@gmail.com>
2025-01-30 05:06:40 +0100

    chore: remove tailwind configs

--- packages/ui-tailwind/.storybook/preview.tsx
+++
@@ -1,6 +0,0 @@
-// import { baseStorybookPreview } from 'config-storybook';
-
-import 'global.css';
-
-// Uncomment when issue is fixed - https://github.com/storybookjs/storybook/issues/23604
-// export default baseStorybookPreview;
--- packages/ui-tailwind/postcss.config.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const config = require('config-tailwind/postcss.config.js');
-
-module.exports = config;
